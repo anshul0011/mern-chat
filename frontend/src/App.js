54fc6421702c71b2aa3b78ba8da8762c59d090a6
anshul0011 <anshul.kumar.22082011@gmail.com>
2022-12-27 22:49:09 +0530

    mern-chat

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+import Chatpage from "./Pages/Chatpage";
+import Homepage from "./Pages/Homepage";
+import { Route } from "react-router-dom";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Route path="/" component={Homepage} exact />
+      <Route path="/chats" component={Chatpage}/>
+    </div>
+  );
+}
+
+export default App;
