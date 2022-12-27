e810a7a51d50bb9b46affb2ca3f8eb59a8ed5e06
anshul0011 <anshul.kumar.22082011@gmail.com>
2022-12-27 22:58:58 +0530

    production is ready

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,60 @@
 const express = require("express");
-const dotenv = require("dotenv");
-const {chats} =require("./data/data");
 const connectDB = require("./config/db");
-const userRoutes= require('./routes/userRoutes');
-const chatRoutes = require('./routes/chatRoutes');
-const messageRoutes = require('./routes/messageRoutes');
+const dotenv = require("dotenv");
+const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const path = require("path");
 
 dotenv.config();
 connectDB();
 const app = express();
-const PORT = process.env.PORT;
 
 app.use(express.json()); // to accept json data
 
-app.get("/", (req, res) => {
-  res.send("API Running vbn!");
-});
+// app.get("/", (req, res) => {
+//   res.send("API Running!");
+// });
 
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
 
-app.use('/api/user',userRoutes);
-app.use('/api/chat',chatRoutes);
-app.use("/api/message",messageRoutes);
+// --------------------------deployment------------------------------
 
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------------deployment------------------------------
+
+// Error Handling middlewares
 app.use(notFound);
 app.use(errorHandler);
 
-const server = app.listen(5000,console.log(`server started ${PORT}`));
+const PORT = process.env.PORT;
+
+const server = app.listen(
+  PORT,
+  console.log(`Server running on PORT ${PORT}...`.yellow.bold)
+);
 
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
     origin: "http://localhost:3000",
-
+    // credentials: true,
   },
 });
 
@@ -67,5 +89,3 @@ io.on("connection", (socket) => {
     socket.leave(userData._id);
   });
 });
-
-
